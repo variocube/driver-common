60053159b02042095c02b2930f6cba29dd86efd2
Peter Karrer <peter@karrer.pro>
2020-08-27 16:37:09 +0200

    device messages can be sent on every driver connection

--- src/common/driver.ts
+++ src/common/driver.ts
@@ -1,4 +1,5 @@
 import {ClientOptions, ControllerClient} from "./client";
+import {DeviceAdded, DeviceMessageTypes, DeviceRemoved, DeviceType} from "./messages";
 
 export enum DriverType {
     Admission = "admission",
@@ -10,5 +11,37 @@ export enum DriverType {
 export class Driver extends ControllerClient {
     constructor(type: DriverType, options?: ClientOptions) {
         super(`/drivers/${type}`, options);
+    }
+
+    /**
+     * Notifies the controller that a device was added.
+     *
+     * @param id The universally unique id of the device.
+     * @param types The device's types
+     * @param vendor The vendor of the device
+     * @param model The device model
+     * @param info Additional device info
+     */
+    async sendDeviceAdded(id: string, types: DeviceType[], vendor: string, model: string, info: any) {
+        await this.client.send<DeviceAdded>({
+            "@type": DeviceMessageTypes.DeviceAdded,
+            id,
+            types,
+            vendor,
+            model,
+            info
+        });
+    }
+
+    /**
+     * Notifies the controller that a device was removed.
+     *
+     * @param id The device id.
+     */
+    async sendDeviceRemoved(id: string) {
+        await this.client.send<DeviceRemoved>({
+            "@type": DeviceMessageTypes.DeviceRemoved,
+            id,
+        });
     }
 }

c2fdd056619d4d7418694224cd7222341b084060
banartdev <office@banart.tech>
2024-11-26 05:25:11 +0100

    chore: remove superfluous messages chore: add .nvmrc

--- src/doorbell/driver.ts
+++ src/doorbell/driver.ts
@@ -1,5 +1,5 @@
 import {ClientOptions, Driver, DriverType} from "../common";
-import { DoorBellMessageTypes, DoorBellRing } from "./messages";
+import {DoorbellMessageTypes, DoorBellRing} from "./messages";
 
 export class DoorBellDriver extends Driver {
 
@@ -8,32 +8,7 @@ export class DoorBellDriver extends Driver {
     }
 
     set onDoorBellRing(handler: (message: DoorBellRing) => Promise<void>) {
-        this.client.on(DoorBellMessageTypes.DoorBellRing, handler);
+        this.client.on(DoorbellMessageTypes.DoorbellRing, handler);
     }
 
-    /**
-     * For Requesting ring to a door
-     * @param serialNr 
-     */
-    async sendDoorBellRing(serialNr: string) {
-        await this.client.send<DoorBellRing>({
-            "@type": DoorBellMessageTypes.DoorBellRing,
-             id: serialNr, 
-        });
-    }
-
-    async sendDoorBellAdded(doorBellId: string) {
-        await this.client.send({
-            "@type": DoorBellMessageTypes.DoorBellAdded,
-            id: doorBellId
-        });
-    }
-
-    async sendDoorBellRemoved(doorBellId: string) {
-        await this.client.send({
-            "@type": DoorBellMessageTypes.DoorBellRemoved,
-            id: doorBellId
-        });
-    }
- 
 }
